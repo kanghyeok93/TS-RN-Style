120554f8f05fcb3db977b1ae4561ba6ee0d9f3a7
smc p-n0055 <kanghyeok93@naver.com>
2023-01-07 14:51:10 +0900

    feat: TS Styling component structure #18

--- src/components/styled/Text.ts
+++ src/components/styled/Text.ts
@@ -1,5 +1,7 @@
 import styled from 'styled-components/native';
 import {IContainerProps} from '../../interfaces/Styled.interface';
+import {Margin, Padding, FontSize, FontWeight} from '../../types/Styled.type';
+import * as Common from './Common';
 
 export const MainText = styled.Text`
   font-size: ${(props: IContainerProps) =>
@@ -8,3 +10,54 @@ export const MainText = styled.Text`
   margin: 10px;
   color: white;
 `;
+
+interface TextType extends Margin, Padding, FontSize, FontWeight {
+  width: number | string;
+  height: number | string;
+  textAlign: string;
+  lineHeight: number;
+  textDecoration: number;
+}
+
+export const Text = styled.Text((props: TextType) => ({
+  // size
+  width: props.width || 'auto',
+  height: props.height || 'auto',
+
+  // AOS의 경우 font family 적용 시 기본적으로 들어가는 마진 값 제거
+  includeFontPadding: 'false',
+
+  // font
+  fontSize: Common.FONT_SIZE(props),
+  fontWeight: Common.FONT_WEIGHT(props),
+  // fontFamily: `NotoSansKR-${Common.FONT_WEIGHT(props)}`,
+
+  // color
+  color: Common.FONT_COLOR(props),
+  backgroundColor: Common.BACKGROUND_COLOR(props),
+
+  // margin
+  marginLeft: Common.MARGIN_LEFT(props),
+  marginRight: Common.MARGIN_RIGHT(props),
+  marginTop: Common.MARGIN_TOP(props),
+  marginBottom: Common.MARGIN_BOTTOM(props),
+
+  // padding
+  paddingLeft: Common.PADDING_LEFT(props),
+  paddingRight: Common.PADDING_RIGHT(props),
+  paddingTop: Common.PADDING_TOP(props),
+  paddingBottom: Common.PADDING_BOTTOM(props),
+
+  margin: props.margin,
+  padding: props.padding,
+
+  //todo 이슈 해결 중
+  // align
+  textAlign: props.textAlign || 'left',
+
+  // text Line
+  textDecoration: props.textDecoration || 'none',
+
+  // line Height
+  lineHeight: props.lineHeight,
+}));

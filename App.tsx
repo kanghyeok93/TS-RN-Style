5f198e21ebdd76ff96be5e780629b7abfd23c73f
smc p-n0055 <kanghyeok93@naver.com>
2023-01-14 18:16:08 +0900

    feat: TS Styling component structure #20

--- App.tsx
+++ App.tsx
@@ -1,17 +1,19 @@
 // src/App.tsx
 import React from 'react';
 import {ThemeProvider} from 'styled-components';
-import {MainText} from './src/components/styled/Text';
-import {Container} from './src/components/styled/View';
+import {Text} from './src/components/styled/Text';
 import Theme from './src/components/styled/theme/Theme';
+import {SafeAreaView} from './src/components/styled/View';
 
 interface Props {}
 const App = ({}: Props) => {
   return (
     <ThemeProvider theme={Theme}>
-      <Container>
-        <MainText>Hello world</MainText>
-      </Container>
+      <SafeAreaView bgLightPink justifyContent={'center'}>
+        <Text ftBlue bold>
+          Styled Component
+        </Text>
+      </SafeAreaView>
     </ThemeProvider>
   );
 };

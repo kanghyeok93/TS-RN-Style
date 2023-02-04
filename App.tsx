69f50e8af79411bf36783ec9a43fc400723d566b
SeoKangHyeok <kanghyeok93@naver.com>
2023-02-05 03:25:52 +0900

    feat: TS Styling component structure #28

--- App.tsx
+++ App.tsx
@@ -3,19 +3,45 @@ import React from 'react';
 import {ThemeProvider} from 'styled-components';
 import {Text} from './src/components/styled/Text';
 import Theme from './src/components/styled/theme/Theme';
-import {SafeAreaView, View} from './src/components/styled/View';
+import {
+  SafeAreaView,
+  View,
+  ViewAbsolute,
+  ViewRow,
+} from './src/components/styled/View';
 
 interface Props {}
 //todo 기본적으로 제공해주는 스타일 속성이 있으므로 스타일 컴포넌트 타입 및 스타일 구조 수정필요
 const App = ({}: Props) => {
   return (
     <ThemeProvider theme={Theme}>
-      <SafeAreaView bgLightPink>
-        <View justifyContent={'center'} alignItems={'center'} flex={1}>
-          <Text ftBlue ftLarge bold>
-            Test
-          </Text>
+      <SafeAreaView bgLightPink alignItems={'center'} justifyContent={'center'}>
+        <ViewRow>
+          <View bgInfo width={150} height={70} padding={10} marginRight={5}>
+            <Text ftWhite ftLarge bold>
+              Test
+            </Text>
+          </View>
+
+          <View bgTomato width={150} height={70} padding={10} marginLeft={5}>
+            <Text ftWhite ftLarge bold>
+              Test
+            </Text>
+          </View>
+        </ViewRow>
+
+        <View
+          borderRadius={15}
+          borderColor={'#FFFFFF'}
+          borderWidth={1}
+          padding={15}
+          marginTop={15}>
+          <Text>ViewBorder</Text>
         </View>
+
+        <ViewAbsolute bgGray bottom={70} padding={30}>
+          <Text ftLime>ViewAbsolute</Text>
+        </ViewAbsolute>
       </SafeAreaView>
     </ThemeProvider>
   );
--- src/components/styled/View.ts
+++ src/components/styled/View.ts
@@ -1,11 +1,5 @@
 import styled from 'styled-components/native';
 import {IContainerProps} from '../../interfaces/Styled.interface';
-import {
-  AlignItems,
-  JustifyContent,
-  Margin,
-  Padding,
-} from '../../types/Styled.type';
 import * as Common from './Common';
 
 export const Container = styled.View`
@@ -16,41 +10,8 @@ export const Container = styled.View`
     props.theme && props.theme.color.black};
 `;
 
-interface ViewType extends Margin, Padding {
-  flex: number;
-  alignItems: AlignItems;
-  justifyContent: JustifyContent;
-  width: number | string;
-  height: number | string;
-  zIndex: number;
-}
-
-export const View = styled.View((props: ViewType) => ({
-  // flex
-
-  // size
-  width: props.width || 'auto',
-  height: props.height || 'auto',
-
-  // color
+export const View = styled.View(props => ({
   backgroundColor: Common.BACKGROUND_COLOR(props),
-
-  // margin
-  margin: Common.MARGIN(props),
-  marginTop: Common.MARGIN_TOP(props),
-  marginRight: Common.MARGIN_RIGHT(props),
-  marginBottom: Common.MARGIN_BOTTOM(props),
-  marginLeft: Common.MARGIN_LEFT(props),
-
-  // padding
-  padding: Common.PADDING(props),
-  paddingTop: Common.PADDING_TOP(props),
-  paddingRight: Common.PADDING_RIGHT(props),
-  paddingBottom: Common.PADDING_BOTTOM(props),
-  paddingLeft: Common.PADDING_LEFT(props),
-
-  // zIndex
-  zIndex: props.zIndex || 1,
 }));
 
 export const SafeAreaView = styled.SafeAreaView(props => ({
@@ -62,31 +23,6 @@ export const ViewRow = styled(View)({
   flexDirection: 'row',
 });
 
-export const ViewAbsolute = styled(View)(props => ({
+export const ViewAbsolute = styled(View)({
   position: 'absolute',
-
-  top: props.top,
-  left: props.left,
-  right: props.right,
-  bottom: props.bottom,
-}));
-
-export const ViewBorder = styled(View)(props => ({
-  borderColor: Common.BORDER_COLOR(props),
-
-  borderWidth: Common.BORDER_WIDTH(props),
-  borderTopWidth: Common.BORDER_TOP_WIDTH(props),
-  borderRightWidth: Common.BORDER_RIGHT_WIDTH(props),
-  borderBottomWidth: Common.BORDER_BOTTOM_WIDTH(props),
-  borderLeftWidth: Common.BORDER_LEFT_WIDTH(props),
-
-  borderRadius: Common.BORDER_RADIUS(props),
-  borderTopLeftRadius: Common.BORDER_TOP_LEFT_RADIUS(props),
-  borderTopRightRadius: Common.BORDER_TOP_RIGHT_RADIUS(props),
-  borderBottomLeftRadius: Common.BORDER_BOTTOM_LEFT_RADIUS(props),
-  borderBottomRightRadius: Common.BORDER_BOTTOM_RIGHT_RADIUS(props),
-}));
-
-export const ViewRowBorder = styled(ViewBorder)({
-  flexDirection: 'row',
 });
